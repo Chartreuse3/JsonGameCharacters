794c5495a16cbd307254a75c4f176363f5ddbdf6
Will Eft <willseft@gmail.com>
2024-10-08 13:55:25 -0500

    [1.07]

---
+++ Program.cs
@@ -0,0 +1,8 @@
+using NLog;
+
+string path = Directory.GetCurrentDirectory() + "//nlog.config";
+// create instance of Logger
+var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+logger.Info("Program started");
+Console.WriteLine("Hello World!");
+logger.Info("Program ended");
