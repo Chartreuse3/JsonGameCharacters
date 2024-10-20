2aa132efd34ac1436770054f82fea4e05e25de0c
Will Eft <willseft@gmail.com>
2024-10-20 18:58:58 -0500

    Added DonkeyKongCharacter class that derived from Character

--- Character.cs
+++ Character.cs
@@ -8,3 +8,13 @@ public abstract class Character
     return $"Id: {Id}\nName: {Name}\nDescription: {Description}\n";
   }
 }
+
+public class DonkeyKongCharacter : Character
+{
+  public string Species { get; set; }
+  
+  public override string Display()
+  {
+    return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nSpecies: {Species}";
+  }
+}
