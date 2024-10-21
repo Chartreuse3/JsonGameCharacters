3683883e9165e82a39d6408f92481b6f9c6a20b1
Will Eft <willseft@gmail.com>
2024-10-20 19:14:02 -0500

    Added street fighter character class, with a list for multiple special move inputs

--- Character.cs
+++ Character.cs
@@ -18,3 +18,14 @@ public class DonkeyKongCharacter : Character
     return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nSpecies: {Species}";
   }
 }
+
+public class StreetFighter : Character
+{
+  public List<string> SpecialMoves { get; set; } = new List<string>();
+
+    public override string Display()
+    {
+      string moves = string.Join(", ", SpecialMoves);
+      return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nSpecial Moves: {string.Join(", ", SpecialMoves)}\n";
+    }
+}
