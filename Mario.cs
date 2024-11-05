91147fc43f03e14c0de712264e1d8d1eb5dacb58
Will Eft <willseft@gmail.com>
2024-11-05 12:37:07 -0600

    Renamed Mario File

--- Mario.cs
+++
@@ -1,8 +0,0 @@
-public class Mario : Character
-{
-  public List<string> Alias { get; set; } = [];
-  public override string Display()
-  {
-    return $"Id: {Id}\nName: {Name}\nDescription: {Description}\nAlias: {string.Join(", ", Alias)}\n";
-  }
-}
